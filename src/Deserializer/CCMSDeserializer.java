490a4b550995f2d77005158ec9be1aeb4b9ce3f4
EfratElmishali <efratharush00@gmail.com>
2015-11-11 11:54:19 +0200

    marge Deserializer
    marge Deserializer

--- src/Deserializer/CCMSDeserializer.java
+++ src/Deserializer/CCMSDeserializer.java
@@ -1,41 +1,30 @@
 package Deserializer;
 
+import global.Manager;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.UUID;
 
 import API.GeneratorAPI;
+import Model.Avail;
 import Model.Event;
 import Model.EventType;
 import Model.ProtocolType;
+import Model.SchDay;
 import Model.ValidateUtils;
 
 public class CCMSDeserializer {
 
 	static String[] rowObjs = new String[15];
 	
-	private static CCMSDeserializer instance = new CCMSDeserializer();
-//	public static boolean checked(String eventRow){
-//
-//		int[] sumArray = {4,6,4,4,3,3,6,6,8,3,11,4,2,4,4};
-//		String[] sCurrentObj = new String[15];
-//
-//		sCurrentObj =  eventRow.split(" ");
-//		int degel = 0;
-//		while(degel < 15){
-//
-//			if(sCurrentObj[degel].length() != sumArray[degel]){
-//				return false;
-//			}
-//
-//			degel++;
-//		}
-//
-//		return true;
-//	}
-	
+	private static CCMSDeserializer instance = new CCMSDeserializer();	
 	
 	private CCMSDeserializer(){}
 	
@@ -52,7 +41,7 @@ public class CCMSDeserializer {
 
 	public boolean validAndConvertRowParams(String eventRow){
 
-		rowObjs = eventRow.split(" ");
+		rowObjs = eventRow.split("\\s+");
 		ValidateUtils.setIProperties(ProtocolType.CCMS);
 
 		date = ValidateUtils.getDate(rowObjs[0]);
@@ -67,7 +56,7 @@ public class CCMSDeserializer {
 		actualPos = ValidateUtils.isValidActualPos(rowObjs[9]);
 		adName = ValidateUtils.getAdName(rowObjs[10]);
 		//ValidateUtils.isValidStat(rowObjs[11]);
-		eventType = ValidateUtils.getEventType(rowObjs[15]);
+		eventType = ValidateUtils.getEventType(rowObjs[14]);
 
 		return ValidateUtils.notNull(date ,time,start ,dur ,brk,pos ,length,actualTime ,actualLength,actualPos,adName ,eventType);
 
@@ -78,28 +67,38 @@ public class CCMSDeserializer {
 
 		BufferedReader br = null;
 
-
 		try {
 
 			String sCurrentLine;
 			File folder = new File("C:\\CCMSDE");
+			Date date = null;
 			for (File fileEntry : folder.listFiles()) {
+				
+				SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
+
+				//try {
+
+					//date = formatter.parse(fileEntry.getName().substring(0, 2));
+
+				//} catch (ParseException e) {
+					//e.printStackTrace();
+				//}
 
 				//create the file
-				//SchDay mySchDay = new SchDay(fileEntry.getName(), ProtocolType.CCMS, new HashMap<String, Event>, new HashMap<String, Avail>, fileEntry.getName().substring(0, 2), fileEntry.getName().substring(2, 4),fileEntry.getName().substring(4, 6),)
-				//  Manager.addFile(mySchDay);
+				SchDay myProtocol = new SchDay(fileEntry.getName(), ProtocolType.CCMS, new HashMap<UUID, Event>(), new HashMap<String, Avail>(), date, (fileEntry.getName().substring(2, 4)),fileEntry.getName().substring(4, 6));
+				Manager.getInstance().addProtocol(myProtocol);
+				
 				System.out.println(fileEntry.getName());
-
+				
 				br = new BufferedReader(new FileReader(fileEntry.getPath()));
 				while ((sCurrentLine = br.readLine()) != null) {
+					
 					System.out.println(sCurrentLine);
 					if(validAndConvertRowParams(sCurrentLine)){
-						Event myEvent = GeneratorAPI.createEvent(date,time,adName,eventType,start,dur,brk,pos,length, fileEntry.getName());
-						//Event myEvent = createEvent(sCurrentObj[0],sCurrentObj[1],sCurrentObj[2],sCurrentObj[3],sCurrentObj[4],sCurrentObj[5],sCurrentObj[6],sCurrentObj[7],sCurrentObj[8],sCurrentObj[9],sCurrentObj[10],sCurrentObj[11],sCurrentObj[12],sCurrentObj[13],sCurrentObj[14], fileEntry.getName())
-						//if(myEvent){
-						//eventMap.add(myEvent);
+						
+						//GeneratorAPI.createEvent(date, time, adName, eventType, start, dur, brk, pos, length, fileEntry.getName());
 						System.out.println("create new event");
-						//}
+						
 					}
 
 					else{
@@ -107,7 +106,6 @@ public class CCMSDeserializer {
 						System.out.println("I can't create new event the num of the digits not ok");
 					}
 				}
-
 			}
 
 		} catch (IOException e) {
