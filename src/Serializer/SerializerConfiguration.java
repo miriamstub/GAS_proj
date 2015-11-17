3abac9d00d92aaa279429dbd4bf334bea236c187
EfratElmishali <efratharush00@gmail.com>
2015-11-17 16:42:43 +0200

    merge
    merge

--- src/Serializer/SerializerConfiguration.java
+++ src/Serializer/SerializerConfiguration.java
@@ -2,7 +2,7 @@ package Serializer;
 
 public class SerializerConfiguration {
 	
-	public static final String FOLDERS_SERIALIZER_PATH = "C:\\";
-	public static final boolean FLAG_OVERIDE_SCHEDULER = false;
+	public static final String FOLDERS_SERIALIZER_PATH = "C:\\CCMS\\";
+	public static final boolean FLAG_OVERIDE_SCHEDULER = true;
 
 }
