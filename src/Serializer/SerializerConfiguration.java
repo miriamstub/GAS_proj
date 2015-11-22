2d40ba035a3d2b86b22bb42cc58e8d91eb48679b
EfratElmishali <efratharush00@gmail.com>
2015-11-22 10:52:38 +0200

    merge
    merge

--- src/Serializer/SerializerConfiguration.java
+++ src/Serializer/SerializerConfiguration.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Serializer;
 
 /**
@@ -12,18 +11,3 @@ public class SerializerConfiguration {
 	public static final String FOLDERS_SERIALIZER_PATH = "C:\\CCMS\\CCMS\\";
 
 }
-=======
-package Serializer;
-
-/**
- * Configuration of Serializer
- * @author eelmisha
- *
- */
-
-public class SerializerConfiguration {
-	
-	public static final String FOLDERS_SERIALIZER_PATH = "C:\\CCMS\\";
-
-}
->>>>>>> refs/remotes/origin/master
