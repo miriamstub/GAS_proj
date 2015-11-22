e2db51acdfe4e142ab4b656889707a98b474ac1c
EfratElmishali <efratharush00@gmail.com>
2015-11-22 12:04:06 +0200

    Fix path
    Fix path

--- src/Serializer/SerializerConfiguration.java
+++ src/Serializer/SerializerConfiguration.java
@@ -8,6 +8,6 @@ package Serializer;
 
 public class SerializerConfiguration {
 	
-	public static final String FOLDERS_SERIALIZER_PATH = "C:\\CCMS\\";
+	public static final String FOLDERS_SERIALIZER_PATH = "C:\\CCMS\\CCMS\\";
 
 }
