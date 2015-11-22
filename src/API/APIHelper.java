f1d760ea8860c0e391ebc12e2decec5c806f4e58
BrachaFeldman <bracha.feldman@harmonicinc.com>
2015-11-22 17:48:02 +0200

    Add assertion: event's object are not null, in validateParams() function.

--- src/API/APIHelper.java
+++ src/API/APIHelper.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.apache.log4j.Logger;
 
+import Model.ConvertAndValidateUtils;
 import Model.ProgramAvail;
 import Model.DateUtils;
 import Model.Event;
@@ -26,12 +27,12 @@ public class APIHelper {
 
 	static Logger logger = Log.getInstance();
 	static Map<String, SchedulerInfo> filesList = Manager.getInstance().getFilesList();
-	
+
 	private static final int MIN_BRK = 1;
 	private static final int MAX_BRK = 999;
 	private static final int MIN_POS = 1;
 	private static final int MAX_POS = 999;
-	
+
 
 	/**
 	 * Validate params of event - the relations between the properties.
@@ -40,7 +41,17 @@ public class APIHelper {
 	 * @return {boolean} true if success
 	 */
 	public static boolean validateParams(Event event, SchedulerInfo schedulerInfo) {
+		
+		//TODO - assert schedulerInfo not null
+		
+		//assert event values are not null
+		if(!ConvertAndValidateUtils.notNull(event.getDate() ,event.getTime(),event.getWindow(),event.getAdName() ,event.getEventType()))
+			return false;
+		
 		Window window = event.getWindow();
+		//assert window values are not null
+		if(!ConvertAndValidateUtils.notNull(window.getDuration(), window.getLength(), window.getStart()))
+			return false;
 
 		if (window.getLength().compareTo(window.getDuration()) == 1) {
 			logger.error("The event length is bigger than the window duration");
@@ -50,7 +61,7 @@ public class APIHelper {
 		// handle SchedulerInfo
 		SchedulerInfo schInfo = filesList.get(schedulerInfo.getSchInfoName());
 		if (schInfo == null) { // create a new file
-				schInfo = new SchDay(schedulerInfo.getSchInfoName(), ((SchDay)schedulerInfo).getDate(), ((SchDay)schedulerInfo).getZone(), ((SchDay)schedulerInfo).getChannel());
+			schInfo = new SchDay(schedulerInfo.getSchInfoName(), ((SchDay)schedulerInfo).getDate(), ((SchDay)schedulerInfo).getZone(), ((SchDay)schedulerInfo).getChannel());
 			filesList.put(schedulerInfo.getSchInfoName(), schInfo);
 		}
 
@@ -61,7 +72,7 @@ public class APIHelper {
 		}
 
 		if (event.getEventType() == EventType.SCHEDULED) { // avail is relevant only for scheduled.
-			
+
 			// assert brk & pos value 
 			if (!(window.getBrk() >= MIN_BRK && window.getBrk() <= MAX_BRK)) {
 				logger.error("The event brk is not in the valid range (" + MIN_BRK + "-"+ MAX_BRK +")");
@@ -76,20 +87,20 @@ public class APIHelper {
 			ProgramAvail avail = filesList.get(schedulerInfo.getSchInfoName()).getAvailMap().get(window.getStart().toString() + window.getDuration().toString());
 
 			if (avail == null ) { // There is no existing avail - need to create a new one!
-		
+
 				// check if overlapping
 				Date windowEndDate = DateUtils.sumDates(window.getStart(), window.getDuration(), 1);
-				
+
 				Calendar calendarStart = Calendar.getInstance();
 				calendarStart.setTime(window.getStart());
 				int hours = calendarStart.get(Calendar.HOUR_OF_DAY);
 				int minutes = calendarStart.get(Calendar.MINUTE);
-				
+
 				Calendar calendarEnd = Calendar.getInstance();
 				calendarEnd.setTime(windowEndDate);
 				int hours1 = calendarEnd.get(Calendar.HOUR_OF_DAY);
 				int minutes1 = calendarEnd.get(Calendar.MINUTE);
-				
+
 				for (int i = hours * 60 + minutes; i < (((hours1 * 60 + minutes1) == 0) ? 1439 : (hours1 * 60 + minutes1 - 1)); i++) {
 					if (schInfo.getOverlappedMins()[i]) {
 						logger.error("This avail overlaped another avail");
@@ -99,7 +110,7 @@ public class APIHelper {
 
 				// fill the array
 				Arrays.fill(schInfo.getOverlappedMins(), hours * 60 + minutes, ((hours1 * 60 + minutes1) == 0) ? 1439 : (hours1 * 60 + minutes1), true);
-				
+
 				avail = new ProgramAvail(window.getStart(), windowEndDate, window.getDuration());
 				filesList.get(schedulerInfo.getSchInfoName()).getAvailMap().put(window.getStart().toString() + window.getDuration().toString(), avail);
 			} else { // exist avail
@@ -112,13 +123,13 @@ public class APIHelper {
 			// set the left duration of the avail.
 			avail.setLeftDuration(DateUtils.sumDates(avail.getLeftDuration(), window.getLength(), -1));
 		}
-		
+
 		// add new key to set
 		schInfo.getEventKeys().add(key);
 
 		return true;
 	}
-	
+
 	/**
 	 * Generate key to the eventKeys of SchedulerInfo
 	 * @param {Window} window
@@ -129,7 +140,7 @@ public class APIHelper {
 	public static String generateKey(Window window, Date eventTime, EventType eventType) {
 		String key = eventType == EventType.SCHEDULED ?
 				"sce" + window.getBrk() + window.getPos() + window.getDuration() + window.getStart() : 
-				"fill" + eventTime;
-		return key;
+					"fill" + eventTime;
+				return key;
 	}
 }
